c1b2e963352f47f8c6afebc2b89400ab3bb14089
owlflame <nsofa701@gmail.com>
2021-10-27 13:45:19 +0400

    First commit

---
+++ vlad/vlad.py
@@ -0,0 +1,27 @@
+#A)
+School = { }
+for i in range(5):
+    School.update( { input(f'Название класса №{i+1}: '): int(input(f'Количество учеников класса № {i+1}: ')) } )
+print(School)
+#B)
+ClassName = input('Введите класс: ')
+if ClassName in School:
+    print(f'Количество учеников в классе { ClassName } : { School[ClassName] } ')
+else:
+    print('Такого класса на существует')
+#C)
+for i in range(3):
+    School.update( { input(f'Название изменяемого класса { i + 1 } : '): int(input(f'Количество учеников изменяемого класса { i + 1 } : ')) } )
+print(School)
+#D)
+for i in range(2):
+    School.update( { input(f'Название нового класса { i + 1 } : '): int(input(f'Количество учеников нового класса { i + 1 } : ')) } )
+print(School)
+#E)
+del School[input(f'Название расформировываемого класса: ')]
+print(School)
+
+
+
+
+
